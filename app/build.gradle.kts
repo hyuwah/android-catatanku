3cbae326463fe6898b49ac64b20e6a48d97df6e1
Muhamad Wahyudin <muhammad.whydn@gmail.com>
2023-08-28 18:22:50 +0700

    (chore): migrate build.gradle to kotlin dsl
    - use version catalog for dependencies management

---
+++ app/build.gradle.kts
@@ -0,0 +1,92 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.kotlin.parcelize)
+}
+
+// [1][0][0][00] ... [9+][9][9][99]
+val versionMajor = 1
+val versionMinor = 1
+val versionPatch = 0
+val versionBuild = 1 // bump for dogfood builds, public betas, etc.
+android {
+    signingConfigs {
+        create("release") {
+            val props = Properties()
+            props.load(FileInputStream(rootProject.file("local.properties")))
+            keyAlias = props.getProperty("keyAlias")
+            keyPassword = props.getProperty("keyPassword")
+            storeFile = file(props.getProperty("storeFile"))
+            storePassword = props.getProperty("storePassword")
+        }
+    }
+    compileSdk = 33
+    defaultConfig {
+        applicationId = "io.github.hyuwah.catatanku"
+        minSdk = 21
+        targetSdk = 33
+        versionCode = (versionMajor * 10000) + (versionMinor * 1000) + (versionPatch * 100) + versionBuild
+        versionName = "${versionMajor}.${versionMinor}.${versionPatch}"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+    buildTypes {
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android.txt"),
+                "proguard-rules.pro"
+            )
+
+            applicationVariants.all {
+                val variant = this
+                variant.outputs.all {
+                    val output = this as com.android.build.gradle.internal.api.BaseVariantOutputImpl
+                    output.outputFileName = "CatatanKu-release-" + defaultConfig.versionName + ".apk"
+                }
+            }
+        }
+    }
+    compileOptions {
+        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+    productFlavors {
+    }
+    namespace = "io.github.hyuwah.catatanku"
+}
+
+kapt {
+    correctErrorTypes = true
+}
+
+dependencies {
+    implementation(libs.kotlin.stdlib.jdk7)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.browser)
+
+    // KTX
+    implementation(libs.core.ktx)
+    implementation(libs.material)
+
+    implementation(libs.android.about.page)
+    implementation(libs.markdownview)
+    implementation(libs.lottie)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
